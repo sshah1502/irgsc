36bf5f8c4a6242eef9248d6471c9933f4f5b03cd
sarang shashikant shah <33885633+sshah1502@users.noreply.github.com>
2023-03-20 18:16:19 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name          = 'irgsctool',
+    version       = '0.0.1',    
+    description   = 'Python tool to generate a catalog of NIR guide stars for the AO observations of the Thirty Meter Telescope.',
+    url           = '',
+    author        = 'Sarang Shah',
+    author_email  = 'sshah1502@gmail.com',
+    license       = 'BSD 2-clause', 
+    package_dir   = {'':'src'},
+    packages      = find_packages(where='src'),          
+    install_requires =['astroquery', 'astropy', 'matplotlib', 'astropy', 'dustmaps.sfd',
+                        'numpy', 'datetime', 'csv', 'json', 'requests', 'pyvo'],
+    include_package_data = True,  
+    package_data       = {'': ['irgsctool/data/*']},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: OS Independent',        
+        'Programming Language :: Python :: 3.6',
+	    'Programming Language :: Python :: 3.9',
+    ],
+   
+)
