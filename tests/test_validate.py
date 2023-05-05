d31312544d02a57d70687c28d0f149e13733b955
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2023-05-05 12:01:01 +0530

    Create test_validate.py

---
+++ tests/test_validate.py
@@ -0,0 +1,16 @@
+"""Test for checking validate module
+"""
+from irgsctool import ValidateIRGSC
+
+v = ValudateIRGSC(0.0,0.0)
+
+def test_read_irgsc():
+  """
+  Simple test to check table integrity as IRGSC 
+  table is being read
+  """
+  
+  tab = v.read_irgsc()
+  
+  assert len(tab)>1
+  assert len(tab.keys())==68
